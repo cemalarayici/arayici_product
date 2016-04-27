f613c46a335b613200ee6b4deeb011e3951b1acd
Arayici <arayici@me.com>
2016-04-27 14:21:10 +0200

    LinkedList

--- Product.java
+++ Product.java
@@ -2,6 +2,7 @@ package sell_arayici;
 
 public class Product{
 	private String name;
+	private float price = 10f;
 	
 	public Product(){
 		setName(name);
--- company.java
+++ company.java
@@ -1,7 +1,7 @@
 package sell_patlak;
 
 public class Company{
-	private ArrayList company;
+	private LinkedList products;
 	private String name = "IBM";
 	
 	public Company(){
